ba0f761d5e723129eab66782d6407d471dec7b48
liqun fu <liqfu@microsoft.com>
2018-05-31 18:59:24 -0700

    test

--- Source/CNTKv2LibraryDll/proto/onnx/core/common/common.h
+++ Source/CNTKv2LibraryDll/proto/onnx/core/common/common.h
@@ -32,36 +32,18 @@
 #include <unordered_map>
 #include <vector>
 
- // make_unique is a C++14 feature. If we don't have 14, we will emulate
- // its behavior. This is copied from folly/Memory.h
-#if __cplusplus >= 201402L ||                                              \
-    (defined __cpp_lib_make_unique && __cpp_lib_make_unique >= 201304L) || \
-    (defined(_MSC_VER) && _MSC_VER >= 1900)
- /* using override */
-using std::make_unique;
+#if defined(__GNUC__) && !defined(__cpp_lib_make_unique)
+namespace std {
+
+    // make_unique was added in GCC 4.9.0. Requires using -std=c++11.
+    template <typename T, typename... Args>
+    unique_ptr<T> make_unique(Args &&... args)
+    {
+        return unique_ptr<T>(new T(forward<Args>(args)...));
+    }
+}
 #else
-
-template <typename T, typename... Args>
-typename std::enable_if<!std::is_array<T>::value, std::unique_ptr<T>>::type
-make_unique(Args&&... args)
-{
-    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-}
-
-// Allows 'make_unique<T[]>(10)'. (N3690 s20.9.1.4 p3-4)
-template <typename T>
-typename std::enable_if<std::is_array<T>::value, std::unique_ptr<T>>::type
-make_unique(const size_t n)
-{
-    return std::unique_ptr<T>(new typename std::remove_extent<T>::type[n]());
-}
-
-// Disallows 'make_unique<T[10]>()'. (N3690 s20.9.1.4 p5)
-template <typename T, typename... Args>
-typename std::enable_if<
-    std::extent<T>::value != 0, std::unique_ptr<T>>::type
-    make_unique(Args&&...) = delete;
-
+using std::make_unique; 
 #endif
 
 #include "proto/onnx/core/common/code_location.h"
